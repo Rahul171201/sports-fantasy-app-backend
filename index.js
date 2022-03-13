ba8596c7e41b61be77471f4f048bc23d16e924de
Rahul Roy <70642284+Rahul171201@users.noreply.github.com>
2022-03-13 11:39:29 +0530

    updated port

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const port = 3000;
+const url = "http://localhost:3000/";
 let express = require('express');
 
+// https://www.tutorialspoint.com/nodejs/nodejs_express_framework.htm
+
 const app = express();
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
     res.send("Finally i learnt some backend");
